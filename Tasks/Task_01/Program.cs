3c958dd107b39b5868ebc4d53a94050f95d0519e
Kaciade <valerypolyakov60@gmail.com>
2023-02-11 13:41:41 +0400

    Задание 1 выполнено

---
+++ Tasks/Task_01/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением
+// y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+Console.WriteLine("y = k1 * x + b1\ny = k2 * x + b2");
+Console.WriteLine();
+
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine();
+Console.WriteLine($"y = ({k1}) * x + ({b1})\ny = ({k2}) * x + ({b2})");
+Console.WriteLine();
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+Console.WriteLine();
